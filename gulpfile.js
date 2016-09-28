a744ca756ef372a8a97e9147e06b38668a12c963
Raymi Saldomando <raymi15@gmail.com>
2016-09-28 17:45:41 -0500

    conflict fix

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,7 @@ gulp.task('serve', function() {
 
     browserSync.init({
         server: "app",
-<<<<<<< HEAD
-        port: 1501
-=======
         port: 1502
->>>>>>> 408342fd17217199f92c3c013f899b3328b2d48e
     });
 
     gulp.watch("clase/css/*.css").on('change', browserSync.reload);
